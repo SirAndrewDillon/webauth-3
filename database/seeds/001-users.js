8ebef7d6510d3ae0cce32dfcf48d46b05c69c073
andrew dillon <majik895@gmail.com>
2019-09-17 13:46:44 -0400

    :hankey: Users database :heavy_check_mark:

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,13 +1,24 @@
+const bcrypt = require('bcryptjs')
+const dillonPassword = bcrypt.hashSync('tacos', 10)
+const ginaPassword = bcrypt.hashSync('steak', 10)
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+	/* ---------------------- Deletes ALL existing entries ---------------------- */
+	return knex('users')
+		.truncate()
+		.then(function() {
+			/* -------------------------- Inserts seed entries -------------------------- */
+			return knex('users').insert([
+				{
+					username: 'Dillon',
+					password: dillonPassword,
+					department: 'Team Edward' //Not Really
+				},
+				{
+					username: 'Gina',
+					password: ginaPassword,
+					department: 'Team Jacob'
+				}
+			])
+		})
+}
